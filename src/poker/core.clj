882a4e7e2873a1cd825be1858020797c98ff45c1
Daniel Gerlach <wrack1981@gmx.de>
2013-12-22 17:08:15 +0100

    started hand-rank and done two-pairs

--- src/poker/core.clj
+++ src/poker/core.clj
@@ -1,4 +1,5 @@
 (ns poker.core
+  (:refer-clojure :exclude [flush])
   (:require (clojure [string :as string]))
   (:gen-class))
 
@@ -15,9 +16,12 @@
    '6C 7C 8C 9C TC' is a straight flush, so the corresponding tuple is:
     (10, 9, 8, 7, 6)"
   [^String hand]
-  (sort > (vec (for [x (string/split hand #"\s+")
-                   :let [y (subs x 0 1)]]
-               (.indexOf rank-values y)))))
+  (let [ranks (sort > (vec (for [x (string/split hand #"\s+")
+                                 :let [y (subs x 0 1)]]
+                             (.indexOf rank-values y))))]
+    (if (= ranks '(14 5 4 3 2))
+      '(5 4 3 2 1)
+      ranks)))
 
 
 (defn straight
@@ -35,15 +39,42 @@
       true
       false)))
 
-(defn kind
-  "Return the first rank that this hand has exactly n of. Return
-   None if there is no n-of-a-kind in the hand."
-  [n ranks]
-  (first (filter (fn [x] (= n (count-freq ranks x))) ranks)))
-
 (defn count-freq
   "Count the frequency of a given element in a sequence"
   [coll elem]
   (reduce #(if (= %2 elem)
              (inc %)
              %) 0 coll))
+
+(defn kind
+  "Return the first rank that this hand has exactly n of. Return
+   None if there is no n-of-a-kind in the hand."
+  [n ranks]
+  (first (filter (fn [x] (= n (count-freq ranks x))) ranks)))
+
+(defn two-pair
+  "If there are two pair, return thw two ranks as a tuple: 
+   (highest, lowest); otherwise return Nil."
+  [ranks]
+  (let [pairs (reduce (fn [x y]
+                        (if (= 2 (count-freq ranks y))
+                          (conj x y)
+                           x)) 
+                      #{}
+                      ranks)]
+    (if (= 2 (count pairs))
+      (sort > pairs)
+      nil)))
+
+(defn hand-rank
+  "Return a value indicating hhow high the hand ranks."
+  [^String hand]
+  (let [ranks (card-ranks hand)]
+    (cond 
+     (and (straight ranks) (flush hand)) (-> [] (conj 8) (conj (apply max ranks)))
+     (kind 4 ranks) (-> [] (conj 7) (conj (kind 4 ranks)) (conj (kind 1 ranks))))))
+
+(defn allmax
+  "Return a list of all items equals to the max of the sequence."
+  [coll key]
+  (apply max-key key coll))
--- test/poker/core_test.clj
+++ test/poker/core_test.clj
@@ -12,7 +12,9 @@
   (testing "calculate the correct card rank of a hand"
     (let [sf "6C 7C 8C 9C TC"
           fk "9D 9H 9S 9C 7D"
-          fh "TD TC TH 7C 7D"]
+          fh "TD TC TH 7C 7D"
+          tp "5S 5D 9H 9C 6S"
+          s1 "AS 2S 3S 4S 5S"]
       (is (= [9, 8, 7, 6, 5] (p/card-ranks "6C 7C 9C 8C 5C")))
       (is (= [14, 13, 12, 11 , 10] (p/card-ranks "TC JC QC KC AC")))
       (is (= [10, 9, 8, 7, 6] (p/card-ranks sf)))
@@ -21,9 +23,16 @@
       (is (true? (p/flush sf)))
       (is (false? (p/flush fk)))
       (is (true? (p/straight [9, 8, 7, 6, 5])))
+      (is (true? (p/straight (p/card-ranks s1))))
       (is (false? (p/straight [9, 8, 8, 6, 5])))
-      (is (= 9 (p/kind p/card-ranks fk))))))
+      (is (= 9 (p/kind 4 (p/card-ranks fk))))
+      (is (= nil (p/kind 3 (p/card-ranks fk))))
+      (is (= nil (p/kind 2 (p/card-ranks fk))))
+      (is (= 7 (p/kind 1 (p/card-ranks fk))))
+      (is (nil? (p/two-pair (p/card-ranks fk))))
+      (is (= '(9 5) (p/two-pair (p/card-ranks tp)))))))
 
 (deftest count-test
   (testing "count the frequency of a given element in a sequence"
-    (is (= 4 (count-freq 9 [9 9 9 9 4])))))
+    (is (= 4 (p/count-freq [9 9 9 9 4] 9)))
+    (is (= 1 (p/count-freq [9 9 9 9 4] 4)))))
